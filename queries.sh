1e6c011320faa07ab33c3cb0140ba38799cd54c6
Dodo <afa1823@hotmail.com>
2025-03-26 17:07:16 -0600

    First commit

--- queries.sh
+++ queries.sh
@@ -25,7 +25,7 @@ echo "$($PSQL "SELECT GREATEST(
     (SELECT MAX(opponent_goals) FROM games)) AS overall_max;")"
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo "$($PSQL "SELECT SUM(winner_goals) FROM games WHERE winner_goals > 2")"
+echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
 echo "$($PSQL "SELECT name FROM teams RIGHT JOIN games ON teams.team_id = games.winner_id WHERE year=2018 AND round='Final'")"
@@ -37,7 +37,7 @@ echo -e "\nList of unique winning team names in the whole data set:"
 echo "$($PSQL "SELECT DISTINCT name FROM teams RIGHT JOIN games ON teams.team_id = games.winner_id ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo "$($PSQL "SELECT year, name FROM teams RIGHT JOIN games ON teams.team_id = games.winner_id WHERE round= 'Final'")"
+echo "$($PSQL "SELECT year, name FROM teams RIGHT JOIN games ON teams.team_id = games.winner_id WHERE round= 'Final' ORDER BY year")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo "$($PSQL "Select name FROM teams WHERE name LIKE 'Co%'")"
+echo "$($PSQL "Select name FROM teams WHERE name LIKE 'Co%' ORDER BY name")"
